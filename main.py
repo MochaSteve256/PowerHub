ab4d83a5009d84ae26ec1f610c10a13120392377
Adrian Steyer <adrian.steyer@posteo.de>
2024-12-30 20:29:37 +0100

    Revert "claudeAI code fix"
    This reverts commit 014e9dc2333f1b65f8db1f3597bdc935a701c34c.

--- main.py
+++ main.py
@@ -6,25 +6,21 @@ import time
 
 press_time = 0
 back_triggered = False
-is_pressed = False  # Track whether the button is currently pressed
 
 def press():
-    global press_time, back_triggered, is_pressed
+    global press_time, back_triggered
     press_time = time.time()
     back_triggered = False
-    is_pressed = True  # Button is now pressed
 
 def update():  # Call this in your main loop
-    global back_triggered, is_pressed
-    if is_pressed and time.time() - press_time >= 0.5 and not back_triggered:
+    global back_triggered
+    if time.time() - press_time >= 0.5 and not back_triggered:
         back_triggered = True
         ui.back()
 
 def release():
-    global is_pressed
-    if is_pressed and time.time() - press_time < 0.5:
+    if time.time() - press_time < 0.5:
         ui.select()
-    is_pressed = False  # Button is no longer pressed
 
 if __name__ == "__main__":
     
