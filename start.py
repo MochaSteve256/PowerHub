d20dd8c087b6f53897bf09dac946e5d434cea722
Adrian Steyer <adrian.steyer@posteo.de>
2025-01-04 09:28:22 +0100

    better start file

--- start.py
+++ start.py
@@ -1,3 +1,27 @@
 import os
+import subprocess
 
-os.system("sudo python3 main.py")
+# Kill old process
+try:
+    result = subprocess.check_output(
+        "ps aux | grep 'python3 main' | grep -v grep | grep -v sudo",
+        shell=True,
+        text=True
+    )
+    if result.strip():
+        pid = result.split()[1]  # The PID is usually the second column in `ps aux` output
+        print(f"Found PID: {pid}")
+        
+        # Step 2: Kill the Process
+        subprocess.run(["sudo kill", pid])
+        print(f"Process {pid} has been killed.")
+    else:
+        print("No matching process found.")
+except subprocess.CalledProcessError as e:
+    print("Failed to find or kill the process:", e)
+
+# Pull changes
+os.system("git pull")
+
+# Start new process
+os.system("nohupsudo python3 main.py")
