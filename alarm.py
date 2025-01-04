e570b25b5514b616770f746c659136f6ee95db0a
Adrian Steyer <adrian.steyer@posteo.de>
2025-01-04 22:50:22 +0100

    logging

--- alarm.py
+++ alarm.py
@@ -12,11 +12,17 @@ class Alarm():
     sunsetTime = "23:00"
     
     def __init__(self, led: led_manager.LED_Stripe) -> None:
+        print("sunrise")
         schedule.every().day.at(self.sunriseTime).do(led.sunrise)
+        print("alarm")
         schedule.every().day.at(self.alarmTime).do(led.alarm)
+        print("cw")
         schedule.every().day.at(self.cwTime).do(led.cold_white)
+        print("black")
         schedule.every().day.at(self.schoolPowerOffTime).do(led.black)
+        print("ww")
         schedule.every().day.at(self.wwTime).do(led.warm_white)
+        print("sunset")
         schedule.every().day.at(self.sunsetTime).do(led.sunset)
     
     def update(self):
